2ce70a00e141d9998c79d54067aa59568033761a
FumiakiSaito <haratairani3000points@gmail.com>
2015-03-28 18:55:47 +0900

    php バージョンチェック git インストールチェック

--- spec/fumiaki.org/php_spec.rb
+++ spec/fumiaki.org/php_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe package('php') do
   it { should be_installed }
-# it { should be_installed.with_version "5.5" }
+  it { should be_installed.with_version "5.5" }
 end
 
-#PHP 設定値確認
+# PHP 設定値確認
 describe 'PHP config parameters' do
   context  php_config('date.timezone') do
     its(:value) { should eq "Asia/Tokyo" }
--- spec/fumiaki.org/yum_spec.rb
+++ spec/fumiaki.org/yum_spec.rb
@@ -4,3 +4,8 @@ describe yumrepo('remi') do
   it { should exist }
 #  it { should_not be_enabled }
 end
+
+describe yumrepo('remi-php55') do
+  it { should exist }
+#  it { should_not be_enabled }
+end
